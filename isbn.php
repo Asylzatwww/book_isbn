28d6995d19aea5305b3a9751de56bf7a36553b4e
topaazaev <asylzatwww@yandex.ru>
2020-09-02 14:20:01 +0600

    html

--- isbn.php
+++ isbn.php
@@ -22,7 +22,7 @@
                 $.ajax({url: "http://asylzat.com/cgi-bin/index.py?findValue=9176371212", success: function(result){
                         $(".result").html(result);
                         var obj = jQuery.parseJSON( result );
-                        console.log( obj.title );
+                        console.log( obj.images );
 
                         $(".content").show();
 
