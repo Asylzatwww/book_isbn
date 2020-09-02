f6194f40d549bc50e02e43a9590f98b7bcd049fe
topaazaev <asylzatwww@yandex.ru>
2020-09-02 12:07:36 +0600

    python api
    link test

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -35,7 +35,7 @@ with requests.Session() as c:
 
     #print(page.content)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content.replace("<!--[if IE 6]>", "").replace("<![endif]-->", ""), 'html.parser')
 
     #print(soup)
 
