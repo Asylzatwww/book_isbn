e058c127dfca837cf57270eec057739e895b0ca5
topaazaev <asylzatwww@yandex.ru>
2020-09-02 12:38:37 +0600

    python api
    link test

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -14,34 +14,21 @@ form = cgi.FieldStorage()
 
 # Get data from fields
 findValue = form.getvalue('findValue')
-#http://biznen.com/cgi-bin/index.py?findValue=4345265460
 
 print("Content-type:text/plain;charset=utf-8\r\n\r\n")
-print(findValue)
+#print(findValue)
 #findValue = '4345265460'
 
 with requests.Session() as c:
-    #url = 'http://www.k-agent.ru/search-of-companies'
     url = 'https://www.amazon.co.uk/s?k=' + findValue
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
-    #r = requests.get(url, headers=headers)
     page = c.get(url, headers=headers)
-    #login_data = dict(tbFindValue=findValue, __EVENTTARGET='lbFindTop', next='/')
-    #print(page.content)
-    #page = c.post(url, data=login_data, headers={"Referer": url})
-
-    #headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
-    #r = requests.get(url, headers=headers)
-
-    #print(page.content)
 
     soup = BeautifulSoup(page.content.replace("<!--[if IE 6]>", "").replace("<![endif]-->", ""), 'html.parser')
 
-    #print(soup)
-
     href= soup.find("h2").find("a", {"class" : "a-link-normal"}).get('href')
 
-    print(href)
+    #print(href)
 
     url = 'https://www.amazon.co.uk' + href
     print(url)
@@ -49,15 +36,7 @@ with requests.Session() as c:
     soup = BeautifulSoup(page.content.replace("<!--[if IE 6]>", "").replace("<![endif]-->", ""), 'html.parser')
 
     #print(soup.title.string)
-    h1 = soup.find("span", {"id" : "productTitle"})
-    print(h1.get_text())
-    json = '{'
-    i = 0
-    for node in soup.find("table", attrs={"class": "compdate"}).find_all("td"):
-        i += 1
-        json += '"field' + str(i) + '" : "' + str(node.findAll(text=True)).replace('"', '').replace("'", "").replace("[", "").replace("]", "") + '", '
-    #print(page.content)
-    json = json[:-2]
-    json += '}'
-    print(json)
-    #print(href)
+    print(soup.find("span", {"id" : "productTitle"}).get_text())
+    print(soup.find("span", {"id" : "productSubtitle"}).get_text())
+
+    print( page.content[ page.content.find("'imageGalleryData' : [") : page.content[ page.content.find("'imageGalleryData' : [") : len(page.content) ].page.content.find("}],") ] )
